6a70f0ea294846f299afc79a9332afc44656b0a0
zalderetepima <zalderete@mail.pima.edu>
2024-06-16 19:00:49 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
-
 # CIS 129 Lab 3 Coffee Shop Simulator
 # This program calculates the total cost of an order per customer input
 # Zach Alderete
 # 6/16/2024
 
-# Prices per items offered
+# Prices per item offered
 coffee_price = 5.00
 muffin_price = 4.00
 tax_rate = 0.06
 
-# Get the number of items requested from user
+# Get the number of items requested from user and convert to integer for calculations
 print("***************************************")
 print("My Coffee and Muffin Shop")
 num_coffees = int(input("Number of coffees bought? "))
@@ -38,10 +32,3 @@ print("6% tax: $ " + format(tax, ".2f"))
 print("---------")
 print("Total: $ " + format(total, ".2f"))
 print("***************************************")
-
-
-# In[ ]:
-
-
-
-
